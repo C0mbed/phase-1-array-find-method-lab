494d6e3e75eafd6bfaa95960610e7055990f5f9b
C0mbed <matt@farncombe.co>
2022-02-07 20:59:29 -0700

    commit tree 1a87e2123a5cf9b0c1352f87c6ac35f687659dac

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const winYear = record.find( ({ result }) => result == 'W')
+    if (winYear) {
+        return winYear.year
+    }  
+}
